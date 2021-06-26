997e726d1871f5fbf4d9ef5f9d94e68b8e7c1691
FooQoo <horseshoes.buzz.17@gmail.com>
2021-06-26 16:24:36 +0900

    時分秒追加

--- functions/test/service/article.service.spec.ts
+++ functions/test/service/article.service.spec.ts
@@ -12,7 +12,9 @@ import MockDate from 'mockdate';
 describe('ArticleService', () => {
   let articleService: ArticleService;
   let articleFireStoreRepository: ArticleFireStoreRepository;
-  Date.now = jest.fn().mockReturnValue(new Date('2021-12-01').valueOf());
+  Date.now = jest
+    .fn()
+    .mockReturnValue(new Date('2021-12-01 09:00:00').valueOf());
 
   beforeEach(async () => {
     const fireStoreConfig = new (FireStoreConfig as jest.Mock)();
@@ -34,13 +36,17 @@ describe('ArticleService', () => {
               'key1',
               'title1',
               'link1',
-              admin.firestore.Timestamp.fromDate(new Date('2021-12-01')),
+              admin.firestore.Timestamp.fromDate(
+                new Date('2021-12-01 09:00:00'),
+              ),
             ),
             new ArticleEntity(
               'key2',
               'title2',
               'link2',
-              admin.firestore.Timestamp.fromDate(new Date('2021-12-02')),
+              admin.firestore.Timestamp.fromDate(
+                new Date('2021-12-02 09:00:00'),
+              ),
             ),
           ];
         });
@@ -49,7 +55,7 @@ describe('ArticleService', () => {
         await articleService.findArticles(new ArticleQuery(1)),
       ).toStrictEqual(
         new ArticleListApiResponse(
-          new InfoResponse('2021-12-01T09:00:00+09:00'), //1638403200
+          new InfoResponse('2021-12-01T09:00:00+09:00'),
           [
             new ArticleResponse('title1', 'link1', 1638316800),
             new ArticleResponse('title2', 'link2', 1638403200),
