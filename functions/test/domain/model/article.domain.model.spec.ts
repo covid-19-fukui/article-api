f02718946daf2e361769831ba019a585bce8b5ee
FooQoo <horseshoes.buzz.17@gmail.com>
2021-07-11 01:12:45 +0900

    テスト修正

--- functions/test/domain/model/article.domain.model.spec.ts
+++ functions/test/domain/model/article.domain.model.spec.ts
@@ -1,9 +1,12 @@
 import Article from '../../../src/domain/model/article.domain.model';
+import UnixTimeStamp from '../../../src/domain/type/unixtimestamp.domain.type';
 
 describe('Article', () => {
   it('ファクトリメソッド', async () => {
-    expect(Article.of('title', 'detail', 1638316800)).toStrictEqual(
-      new Article('title', 'detail', 1638316800),
+    expect(
+      Article.of('title', 'detail', new UnixTimeStamp(1638316800)),
+    ).toStrictEqual(
+      new Article('title', 'detail', new UnixTimeStamp(1638316800)),
     );
   });
 });
